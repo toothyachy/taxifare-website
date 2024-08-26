e2e4dc61c6903ac147fb495b7bda31a4d4005047
Karen Ann <kamxlee@gmail.com>
2024-08-26 15:05:27 +0800

    Added mapbox geocoder

--- app.py
+++ app.py
@@ -1,6 +1,47 @@
 import streamlit as st
 import datetime
 import requests
+import geocoder
+
+
+url = st.secrets['url']
+# url = 'https://taxifare.lewagon.ai/predict'
+
+st.title('NY Fare Prediction')
+
+with st.form('predict'):
+    d = st.date_input("Date of ride", datetime.date(2020, 7, 6))
+    t = st.time_input("Time of ride", datetime.time(19,30))
+    pickup_location=st.text_input("Pickup location", value=('Central Park'))
+    dropoff_location=st.text_input("Dropoff location", value=('Chinatown'))
+    # pickup_longitude = st.number_input("Pickup longitude", value=(-73.950655))
+    # pickup_latitude = st.number_input("Pickup latitude", value=(40.783282))
+    # dropoff_longitude = st.number_input("Dropoff longitude", value=(-73.984365))
+    # dropoff_latitude = st.number_input("Dropoff latitude", value=(40.769802))
+    passenger_count = st.number_input("Number of passengers", value=(2))
+    pickup_datetime = f"{d} {t}"
+
+    submit = st.form_submit_button('predict fare')
+
+
+if submit:
+    with st.spinner('Computing fare...'):
+        pickup = geocoder.mapbox(f"{pickup_location},NY", key=st.secrets['mapbox'])
+        dropoff = geocoder.mapbox(f"{dropoff_location},NY", key=st.secrets['mapbox'])
+
+        params = {
+            "pickup_datetime": pickup_datetime,
+            "pickup_longitude": pickup.json['lng'],
+            "pickup_latitude": pickup.json['lat'],
+            "dropoff_longitude": dropoff.json['lng'],
+            "dropoff_latitude": dropoff.json['lat'],
+            "passenger_count": passenger_count
+        }
+
+        response = requests.get(url, params=params)
+        fare = response.json()
+        st.info(f"Predicted Fare is: {fare['fare']}", icon="🚕")
+
 
 '''
 # TaxiFareModel front
@@ -23,38 +64,6 @@ Either as with the title by just creating a string (or an f-string). Or as with 
 - dropoff latitude
 - passenger count
 '''
-
-url = 'https://taxifare-239305196180.europe-west1.run.app/predict'
-# url = 'https://taxifare.lewagon.ai/predict'
-
-st.title('Fare Prediction')
-
-with st.form('predict'):
-    d = st.date_input("Date of ride", datetime.date(2020, 7, 6))
-    t = st.time_input("Time of ride", datetime.time(19,30))
-    pickup_longitude = st.number_input("Pickup longitude", value=(-73.950655))
-    pickup_latitude = st.number_input("Pickup latitude", value=(40.783282))
-    dropoff_longitude = st.number_input("Dropoff longitude", value=(-73.984365))
-    dropoff_latitude = st.number_input("Dropoff latitude", value=(40.769802))
-    passenger_count = st.number_input("Number of passengers", value=(2))
-    pickup_datetime = f"{d} {t}"
-    submit = st.form_submit_button('predict fare')
-
-
-if submit:
-    with st.spinner('Computing fare...'):
-        params = {
-            "pickup_datetime": pickup_datetime,
-            "pickup_longitude": pickup_longitude,
-            "pickup_latitude": pickup_latitude,
-            "dropoff_longitude": dropoff_longitude,
-            "dropoff_latitude": dropoff_latitude,
-            "passenger_count": passenger_count
-        }
-        response = requests.get(url, params=params)
-        fare = response.json()
-        st.info(f"Predicted Fare is: {fare['fare']}", icon="🚕")
-
 
 '''
 ## Once we have these, let's call our API in order to retrieve a prediction
